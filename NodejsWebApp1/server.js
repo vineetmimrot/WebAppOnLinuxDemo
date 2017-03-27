9eb36e6e2b3d98107a6c36815e0ff8b2f92036d7
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-03-27 14:24:12 +0530

    Update server.js

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,5 +4,5 @@ var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Demo for Build 1\n');
+    res.end('Hello World Demo for Build 2\n');
 }).listen(port);
