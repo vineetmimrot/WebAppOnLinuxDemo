0bd389886f2d9a9f11e305b644943a179e9ddfc4
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-04-13 18:37:14 +0530

    Update server.js

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,8 @@
 'use strict';
 var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 80;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Demo for port 1337 \n');
+    res.end('Hello World Demo for port 80 \n');
 }).listen(port);
