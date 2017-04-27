751ba3c7ce2c843883414e4bf3db7f1ebbf1947a
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-04-27 15:09:24 +0530

    Update server.js

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Demo for Build 2017\n');
+    res.end('Hello World Demo for Build 2017 - auto image update\n');
 }).listen(port);
