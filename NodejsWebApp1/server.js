27bba3672e339c68d5f0ef3936723b41e274b253
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-05-09 12:46:52 +0530

    Update server.js

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,8 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1137;
+var port = process.env.PORT || 80;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Demo for Build 2017 - updated from App Service Deploy task : AtulM\n');
+    res.end('Hello World Demo for Build 2017 - updated from App Service Deploy task\n');
 }).listen(port);
