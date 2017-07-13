4720741725cc8d767bd9f85db9215b5f510ec3e8
Amal Joe Gladwin Maria Antony <amaljg@users.noreply.github.com>
2017-07-13 16:58:01 +0530

    Update server.js

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 80;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Demo for Build 2017 - updated from App Service Deploy task (test edit)\n');
+    res.end('Hello World Demo for Deploying NodeJs Container App to Linux App-Service');
 }).listen(port);
